48da78a8b120d2a4dacac48696e33a51c62f0671
Chris Ware <c.p.ware@hotmail.com>
2025-10-23 07:33:29 -0700

    About us done

--- src/components/HomePage/AboutUs/AboutUs.tsx
+++ src/components/HomePage/AboutUs/AboutUs.tsx
@@ -1,17 +1,58 @@
-import LayoutWrapper from '@/components/shared/LayoutWrapper'
-import styles from './AboutUs.module.css';
-import Image from 'next/image';
-import Img1 from '../../../../public/images/about1.jpg'
-import Img2 from '../../../../public/images/about2.jpg'
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+import styles from "./AboutUs.module.css";
+import Image from "next/image";
+import Img1 from "../../../../public/images/about1.jpg";
+import Img2 from "../../../../public/images/about2.jpg";
+import SectionHeading from "@/components/shared/SectionHeading/SectionHeading";
+import Button from "@/components/shared/Button/Button";
 
 export default function AboutUs() {
   return (
     <div className={styles.container}>
-        <LayoutWrapper>
-            <div className={styles.content}>
-
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <SectionHeading text='About' />
+            <h2 className={styles.heading}>Meet Velvet & Vine</h2>
+            <SectionHeading text='Hair Stylists' />
+            <p className={styles.copy}>
+              At Velvet & Vine, we believe in the power of nature to transform
+              your hair. Our products are crafted with the finest botanical
+              ingredients, ensuring that your hair receives the nourishment it
+              deserves.
+            </p>
+            <div className={styles.btnContainer}>
+              <Button href='/' text='Moer About Us' btnType='tanBorder' />
             </div>
-        </LayoutWrapper>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.r1}>
+              <div className={styles.imgContainer}>
+                <Image src={Img1} alt='About us' fill className={styles.img} />
+              </div>
+            </div>
+            <div className={styles.r2}>
+              <div className={styles.imgContainerii}>
+                <Image
+                  src={Img2}
+                  alt='About us 2'
+                  fill
+                  className={styles.img}
+                />
+              </div>
+              <div className={styles.box}>
+                <h3 className={styles.subheading}>
+                  Empowering your <br className={styles.br} /> vision
+                </h3>
+                <br />
+                <p className={styles.copyii}>
+                  Transforming challenges into opportunities
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutWrapper>
     </div>
-  )
+  );
 }
