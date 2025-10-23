0370390b0899c739b41f93e38815a9e4c40e9e5f
Chris Ware <c.p.ware@hotmail.com>
2025-10-22 23:12:30 -0700

    About us start

---
+++ src/components/HomePage/AboutUs/AboutUs.tsx
@@ -0,0 +1,17 @@
+import LayoutWrapper from '@/components/shared/LayoutWrapper'
+import styles from './AboutUs.module.css';
+import Image from 'next/image';
+import Img1 from '../../../../public/images/about1.jpg'
+import Img2 from '../../../../public/images/about2.jpg'
+
+export default function AboutUs() {
+  return (
+    <div className={styles.container}>
+        <LayoutWrapper>
+            <div className={styles.content}>
+
+            </div>
+        </LayoutWrapper>
+    </div>
+  )
+}
