a6963983d97a37953d8fb23b26a39180a5040514
Chris Ware <c.p.ware@hotmail.com>
2025-10-23 23:51:36 -0700

    AboutUsDetails done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutPageIntro from "@/components/AboutPage/AboutPageIntro/AboutPageIntro";
+import AboutUsDetails from "@/components/AboutPage/AboutUsDetails/AboutUsDetails";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutPageIntro />
+      <AboutUsDetails />
     </main>
   );
 }
---
+++ src/components/AboutPage/AboutUsDetails/AboutUsDetails.tsx
@@ -0,0 +1,29 @@
+import SectionHeading from "@/components/shared/SectionHeading/SectionHeading";
+import styles from "./AboutUsDetails.module.css";
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+
+export default function AboutUsDetails() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.imgOverlay} />
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.sectionHeadingContainer}>
+            <SectionHeading text='More About Us' color='tan' />
+          </div>
+          <h2 className={styles.heading}>
+            &ldquo;We are passionate about creating beautiful hair. We believe
+            in providing a personalized experience, listening to your needs, and
+            using high-quality products to achieve stunning results.&rdquo;
+          </h2>
+          <p className={styles.copy}>
+            Our team of skilled and creative stylists is dedicated to delivering
+            exceptional service, from the latest trends to timeless classics.
+            Whether you&apos;re here for a fresh cut, vibrant color, or a
+            signature style, we aim to make every visit relaxing, inspiring.
+          </p>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+}
