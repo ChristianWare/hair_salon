997453c4ed4d6666c4e60d0738f5c7e85ffc4919
Chris Ware <c.p.ware@hotmail.com>
2025-10-27 15:46:28 -0700

    book appointment page start

--- src/app/book-appointment/page.tsx
+++ src/app/book-appointment/page.tsx
@@ -1,9 +1,15 @@
 import BookAppointmentIntro from "@/components/BookAppointment/BookAppointmentIntro/BookAppointmentIntro";
+import FinalCTA from "@/components/shared/FinalCTA/FinalCTA";
+import Footerii from "@/components/shared/Footerii/Footerii";
+import InstaFeed from "@/components/shared/InstaFeed/InstaFeed";
 
 export default function BookAppointmentPage() {
   return (
     <main>
       <BookAppointmentIntro />
+      <FinalCTA />
+      <InstaFeed />
+      <Footerii />
     </main>
   );
 }
